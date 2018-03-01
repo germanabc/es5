58ae92755facdbbc88bade54220b72619effd88c
baocheng <2452440116@qq.com>
2018-03-01 14:21:55 +0800

    删除多余的

--- 柯松/js/ie8.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by admin on 2015/11/9.
- */
-
-$(document).ready(function(){
-    $(".flip-container .back").css("display","none");
-});
--- 柯松/xzxjy/js/html5.js
+++
@@ -1,7 +0,0 @@
-(function(){
-            var element=['header','footer','article','aside','section','nav','menu','hgroup','details','dialog','figure','figcaption'],
-                len=element.length;
-            while(len--){
-                document.createElement(element[len])
-            }
- })();
